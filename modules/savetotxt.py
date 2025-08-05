b0599adb914efc1de48139eab8d5bc389bc5db92
Rami Ben Amor <155146849+RamiBenAmor@users.noreply.github.com>
2025-08-05 14:00:36 +0100

    Add files via upload

---
+++ modules/savetotxt.py
@@ -0,0 +1,12 @@
+
+import os
+
+def savetotxt(text, filename):
+    output_path = "C:\\Users\\ramib\\OneDrive\\Bureau\\CV_RH\\results"
+    os.makedirs(output_path, exist_ok=True)
+
+    full_path = os.path.join(output_path, filename)
+
+    # Écriture en UTF-8 pour bien gérer les caractères spéciaux
+    with open(full_path, "w", encoding="utf-8") as f:
+        f.write(text)
