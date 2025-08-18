7259526fcda241fa893d5e9cbfcbb008d8aae007
Rami Ben Amor <155146849+RamiBenAmor@users.noreply.github.com>
2025-08-18 15:58:00 +0100

    Update classifier.py

--- modules/classifier.py
+++ modules/classifier.py
@@ -2,7 +2,7 @@ from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
 # Charger modèle + tokenizer
-model_path = "C:\\Users\\ramib\\OneDrive\\Bureau\\proj CV_RH\\models\\bert_base_uncased_Fine_Tuned"
+model_path = "{Your path}\\Smart-AI-RH-Assistant\\models\\bert_base_uncased_Fine_Tuned"
 tokenizer = BertTokenizer.from_pretrained(model_path)
 model = BertForSequenceClassification.from_pretrained(model_path)
 model.eval()
@@ -22,4 +22,5 @@ with torch.no_grad():
 
 # Traduire en label texte (avec LabelEncoder)
 predicted_label =le.inverse_transform([predicted_class_id])[0]
+
 print(f"Le texte est classé dans la catégorie : {predicted_label}")
